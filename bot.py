58783e21420cea899424ed9ec290343cbe376a0a
astolfo-maker <kolyazv1829@gmail.com>
2026-03-28 12:44:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
- import telebot; 
-bot = telebot.TeleBot('%8135298684:AAGaOUd-THoiNkZpSE7m9xxi799v-M6fjeI%');
+import telebot
+import os
 
-@bot.message_handler(content_types=['text']) 
-def get_text_messages(message):
-    if message.text == "/start": 
-        bot.send_message(message.from_user.id, "https://t.me/Fnmby_bot/Femboyleggs_bot") 
+# Вставь сюда НОВЫЙ токен (БЕЗ кавычек %)
+BOT_TOKEN = "8135298684:AAGaOUd-THoiNkZpSE7m9xxi799v-M6fjeI"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    # Отправляем сообщение с кнопкой или просто ссылку
+    bot.send_message(message.chat.id, "Привет жирный толстый пухлый, твоя ссылка: https://t.me/Fnmby_bot/Femboyleggs_bot")
+
+# ЭТОЙ СТРОКИ НЕ ХВАТАЛО - ОНА ЗАПУСКАЕТ БОТА
+if __name__ == '__main__':
+    print("Бот запущен...")
+    bot.infinity_polling()
